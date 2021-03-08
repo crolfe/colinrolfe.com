4e6233a7bcd8233fee597d44ea0b6e2a767b6eb5
Colin Rolfe <crolfe@gmail.com>
2021-03-08 02:02:01 +0000

    optionally enable watch/serve plugins based on NODE_ENV

--- index.js
+++ index.js
@@ -4,20 +4,17 @@ var Metalsmith = require('metalsmith'),
     markdown   = require('metalsmith-markdown'),
     assets     = require('metalsmith-assets'),
     ignore     = require('metalsmith-ignore'),
+    msIf       = require('metalsmith-if'),
     serve      = require('metalsmith-serve'),
     watch      = require('metalsmith-watch');
 
 var devBuild = ((process.env.NODE_ENV || '').trim().toLowerCase() === 'dev');
 console.log("devBuild is " + devBuild);
 
-Metalsmith(__dirname)
+var site = Metalsmith(__dirname)
   .metadata({
-        title: "My Static Site & Blog",
-        description: "It's about saying »Hello« to the World.",
-        generator: "Metalsmith",
-        url: "http://www.metalsmith.io/",
-        devBuild: devBuild
-      })
+      devBuild: devBuild
+  })
   .source('./src')
   .destination('./build')
   .clean(false)
@@ -31,20 +28,26 @@ Metalsmith(__dirname)
     'lib/**'
   ]))
   .use(layouts({
-        engine: 'handlebars'
-      }))
-  .use(serve({
-    host: '0.0.0.0',
-    port: 8080
+    engine: 'handlebars'
   }))
-  .use(watch({
-    paths: {
-      '${source}/**/*': "**/*",
-      './layouts/**/*': "**/*",
-      './index.js': "**/*"
-    },
-    livereload: true
-  }))
+  .use(msIf(
+    // enabled only when devBuild is true
+    devBuild,
+    serve({
+      host: '0.0.0.0',
+      port: 8080
+  })))
+  .use(msIf(
+    // enabled only when devBuild is true
+    devBuild,
+    watch({
+      paths: {
+        '${source}/**/*': "**/*",
+        './layouts/**/*': "**/*",
+        './index.js': "**/*"
+      },
+      livereload: devBuild
+  })))
   .build(function(err, files) {
     if (err) {
       throw err;
